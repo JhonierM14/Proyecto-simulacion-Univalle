0c2de8c49f5d49c9fe192eae4fd8f0f2999ec7e5
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-04-22 23:34:52 -0500

    Update Newton.py

--- Newton.py
+++ Newton.py
@@ -21,13 +21,13 @@ cantidadDecimales = 8
 
 # Newton
 iteraciones = 5
-presicion = pow(10, -6)
+presicion = pow(10, -5)
 
 MatrizVelocidadEjeX = np.zeros((filas, columnas), float)
 MatrizVelocidadEjeY = np.zeros((filas, columnas), float)
 
 # configuracion de numpy que me permite ver la matriz jacobiana sin salto de linea
-np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # se intenta evitar saltos de línea
+np.set_printoptions(suppress=True, precision=cantidadDecimales, threshold=np.inf, linewidth=np.inf)  # se intenta evitar saltos de línea
 
 # valores de frontera
 def ValsFrontMatrizVel(MatrizVelocidadEjeX):
@@ -67,104 +67,101 @@ def ecuacionDiscretizada(Vx0, Vy0, CSup, CIzq, CInf, CDer, vij):
     f = 1/4*(-Vx0*coeficiente1 - Vy0*coeficiente2 + sumaCasillasAdyacentes) - vij
     return round(f, cantidadDecimales)
 
-contador = 0
-while contador<=iteraciones:
+def llenarMatrizF(MatrizVelocidadEjeX, MatrizVelocidadEjeY):
     F = np.zeros((filas - 2 , columnas - 2), float)
-    def llenarMatrizF(F):
-        for i in range(filas-2):
-            for j in range(columnas-2):
-                vx = MatrizVelocidadEjeX[i+1, j+1]
-                vSup = MatrizVelocidadEjeX[i, j+1]
-                vIzq = MatrizVelocidadEjeX[i+1, j] 
-                vy = MatrizVelocidadEjeY[i+1, j+1]
-                vInf = MatrizVelocidadEjeX[i+2, j+1] 
-                vDer = MatrizVelocidadEjeX[i+1, j+2]
-                vij = MatrizVelocidadEjeX[i+1, j+1]
+    for i in range(filas-2):
+        for j in range(columnas-2):
+            vx = MatrizVelocidadEjeX[i+1, j+1]
+            vSup = MatrizVelocidadEjeX[i, j+1]
+            vIzq = MatrizVelocidadEjeX[i+1, j] 
+            vy = MatrizVelocidadEjeY[i+1, j+1]
+            vInf = MatrizVelocidadEjeX[i+2, j+1] 
+            vDer = MatrizVelocidadEjeX[i+1, j+2]
+            vij = MatrizVelocidadEjeX[i+1, j+1]
 
-                F[i,j] = round(ecuacionDiscretizada(vx, vy, vSup, vIzq, vInf, vDer, vij), cantidadDecimales)
-        
-        vector = F.ravel()
-        return vector
+            F[i,j] = round(ecuacionDiscretizada(vx, vy, vSup, vIzq, vInf, vDer, vij), cantidadDecimales)
+    
+    vector = F.ravel()
+    return vector
 
-    F = llenarMatrizF(F)
+# matriz jacobiana
+# todas las variables van a estar representadas en una sola fila, pero solo 
+# 5 valores valores van a tener coeficiente distinto de 0, que es la cantidad
+# de variables independientes en la ecuacion que representa cada casilla
 
-    # matriz jacobiana
-    # todas las variables van a estar representadas en una sola fila, pero solo 
-    # 5 valores valores van a tener coeficiente distinto de 0, que es la cantidad
-    # de variables independientes en la ecuacion que representa cada casilla
+# matriz jacobiana de F
+tamaño = (filas-2)*(columnas-2)
+J = np.zeros((tamaño, tamaño), float)
 
-    # matriz jacobiana de F
-    tamaño = (filas-2)*(columnas-2)
-    J = np.zeros((tamaño, tamaño), float)
+# las filas del jacobiano representan la cantidad de ecuaciones, es decir la cantidad
+# de filas que debe tener el jacobiano es igual a 3x9
 
-    # las filas del jacobiano representan la cantidad de ecuaciones, es decir la cantidad
-    # de filas que debe tener el jacobiano es igual a 3x9
+# las columnas del jacobiano representan las derivadas respecto a las variables independientes, es decir
+# el numero de columas debe ser igual al numero de filas
 
-    # las columnas del jacobiano representan las derivadas respecto a las variables independientes, es decir
-    # el numero de columas debe ser igual al numero de filas
+# configuracion de numpy que me permite ver la matriz jacobiana sin salto de linea
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # se intenta evitar saltos de línea
 
-    # configuracion de numpy que me permite ver la matriz jacobiana sin salto de linea
-    np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # se intenta evitar saltos de línea
+# la formula del metodo de Newton es J^(-1)xH = -F al pasar a multiplicar por el inverso de J^(-1)
+# nos queda H = -J^(-1)xF, por lo tanto se tendra en cuenta el signo negativo de una vez
+# al momento de llenar la matriz jacobiana
 
-    # la formula del metodo de Newton es J^(-1)xH = -F al pasar a multiplicar por el inverso de J^(-1)
-    # nos queda H = -J^(-1)xF, por lo tanto se tendra en cuenta el signo negativo de una vez
-    # al momento de llenar la matriz jacobiana
+def MatrizJacobiana(M_V_x, M_V_y):
+    filas_internas = filas - 2
+    columnas_internas = columnas - 2
+    tam = filas_internas * columnas_internas
+    J = np.zeros((tam, tam), float)
 
-    def MatrizJacobiana(M_V_x, M_V_y):
-        filas_internas = filas - 2
-        columnas_internas = columnas - 2
-        tam = filas_internas * columnas_internas
-        J = np.zeros((tam, tam), float)
+    def pos(i, j):
+        return i * columnas_internas + j
 
-        def pos(i, j):
-            return i * columnas_internas + j
+    for i in range(filas_internas):
+        for j in range(columnas_internas):
+            fila_eq = pos(i, j)
+            vx = M_V_x[i+1, j+1]
+            vy = M_V_y[i+1, j+1]
 
-        for i in range(filas_internas):
-            for j in range(columnas_internas):
-                fila_eq = pos(i, j)
-                vx = M_V_x[i+1, j+1]
-                vy = M_V_y[i+1, j+1]
+            # Derivadas parciales
 
-                # Derivadas parciales
+            # dv/d(v_ij)
+            # J[fila_eq, fila_eq] = 1/4*((M_V_x[i+2, j+1]- M_V_x[i, j+1]))/2 - 1
+            J[fila_eq, fila_eq] = - 1
 
-                # dv/d(v_ij)
-                # J[fila_eq, fila_eq] = 1/4*((M_V_x[i+2, j+1]- M_V_x[i, j+1]))/2 - 1
-                J[fila_eq, fila_eq] = - 1
+            # dv/d(v_{i-1,j})
+            if i > 0:
+                J[fila_eq, pos(i-1, j)] = 0.25 * (vx / 2 + 1)
 
-                # dv/d(v_{i-1,j})
-                if i > 0:
-                    J[fila_eq, pos(i-1, j)] = 0.25 * (vx / 2 + 1)
+            # dv/d(v_{i+1,j})
+            if i < filas_internas - 1:
+                J[fila_eq, pos(i+1, j)] = 0.25 * (-vx / 2 + 1)
 
-                # dv/d(v_{i+1,j})
-                if i < filas_internas - 1:
-                    J[fila_eq, pos(i+1, j)] = 0.25 * (-vx / 2 + 1)
+            # dv/d(v_{i,j-1})
+            if j > 0:
+                J[fila_eq, pos(i, j-1)] = 0.25 * (vy / 2 + 1)
 
-                # dv/d(v_{i,j-1})
-                if j > 0:
-                    J[fila_eq, pos(i, j-1)] = 0.25 * (vy / 2 + 1)
+            # dv/d(v_{i,j+1})
+            if j < columnas_internas - 1:
+                J[fila_eq, pos(i, j+1)] = 0.25 * (-vy / 2 + 1)
 
-                # dv/d(v_{i,j+1})
-                if j < columnas_internas - 1:
-                    J[fila_eq, pos(i, j+1)] = 0.25 * (-vy / 2 + 1)
-
-        return J
-
-    J = MatrizJacobiana(MatrizVelocidadEjeX, MatrizVelocidadEjeY)
+    return J
 
     ###########################################################################################
     #                                       SOLUCION
     ###########################################################################################
 
+contador = 0
+while contador<=iteraciones:
+    F = llenarMatrizF(MatrizVelocidadEjeX, MatrizVelocidadEjeY)
+    J = MatrizJacobiana(MatrizVelocidadEjeX, MatrizVelocidadEjeY)
+    if np.linalg.det(J) == 0:
+        raise ValueError("La matriz Jacobiana es singular. Newton no puede continuar.")
+
     # se resuelve iterativamente la ecuacion H = -J^(-1)xF que es equivalente a
     # Xn+1 = Xn - J^(-1)xF
 
-    # se tienen las matrices F Y J
-    def valoresSolucion(J, F, cantidad_decimales):
-        delta_x = np.linalg.solve(J, -F)
-        delta_x = np.round(delta_x, cantidad_decimales)  # Ajuste de decimales
-        return delta_x
-
-    H = valoresSolucion(J, F, cantidadDecimales)
+    #  J * H = -F
+    H = np.linalg.solve(J, -F)
+    H = np.round(H, cantidadDecimales)
 
     ###########################################################################################
     #                                  Valores de la velocidad
@@ -191,6 +188,9 @@ while contador<=iteraciones:
     if delta < presicion or contador >= iteraciones:
         print(f"Convergencia alcanzada en iteración {contador} con delta = {delta:.6f}")
         
+        # valores solucion al sistema de ecuaciones
+        print(np.array2string(MatrizVelocidadEjeX, separator=', ', threshold=np.inf))
+
         grafico_matriz_velocidad(MatrizVelocidadEjeX)
 
         break
